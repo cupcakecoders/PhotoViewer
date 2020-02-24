79e15eec2e41644eae86b292a51524596be3ae57
Beatrice <beatrice.opara@softwire.com>
2020-02-24 15:08:56 +0000

    added state and useState hook to display the current URL of the image

--- src/ThumbnailsImages/ThumbnailsImages.js
+++ src/ThumbnailsImages/ThumbnailsImages.js
@@ -1,18 +1,26 @@
-﻿import React from "react";
+﻿import React, {useState} from "react";
 import {ImageUrls} from "../data";
 import "./ThumbnailsImages.css";
+
+
+
+//display current URL 
+//add 'onClick' event handler that updates the state when you click on thumbnail 
 
 function ThumbnailImages(){
     const listOfImages = [];
     
+    const [imageURL, getImageURL] = useState("");
+    
     for (let i = 0; i < ImageUrls.length; i++){
         const image = ImageUrls[i];
-        listOfImages.push(<li><img src = {image}/></li>);
+        listOfImages.push(<li><img src = {image} onClick={()=> getImageURL (image)}/></li>);
     }
     return (
         <div>
            <ul>
-               {listOfImages}
+                Selected image: {imageURL}
+                {listOfImages}
            </ul>
         </div>
     );
